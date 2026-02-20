7a87f90c7ca2bac07d620b19102ac59eae9ad4b2
quantakavin <quantakavin@gmail.com>
2026-02-20 21:08:38 +0000

    exclude sections

--- rag_service/src/ingestion/section_detect.py
+++ rag_service/src/ingestion/section_detect.py
@@ -7,3 +7,24 @@ from typing import Any
 from ..utils.logger import setup_logger
 
 logger = setup_logger(__name__)
+
+EXCLUDED_SECTIONS = {
+    "authors",
+    "author",
+    "contributors",
+    "affiliations",
+    "references",
+    "bibliography",
+    "citations",
+    "works cited",
+    "acknowledgments",
+    "acknowledgements",
+    "disclosures",
+    "conflicts of interest",
+    "conflict of interest",
+    "appendix",
+    "appendices",
+}
+
+BULLET_PATTERN = re.compile(r"^[-•\d]")
+NUMBERED_HEADING_PATTERN = re.compile(r"^(\d+(\.\d+)*\.?)\s+([A-Z][a-zA-Z\s]{2,})$")
