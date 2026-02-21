0508e5a3ae469b2665b3488e93ee40f900a89ef7
quantakavin <quantakavin@gmail.com>
2026-02-21 23:59:30 +0000

    add constants for chunking

--- rag_service/src/ingestion/chunk.py
+++ rag_service/src/ingestion/chunk.py
@@ -3,6 +3,26 @@ from __future__ import annotations
 import hashlib
 from typing import Any
 
+import nltk
+import tiktoken
+
 from ..utils.logger import setup_logger
 
+nltk.download("punkt", quiet=True)
+nltk.download("punkt_tab", quiet=True)
+from nltk.tokenize import sent_tokenize  # noqa: E402
+
 logger = setup_logger(__name__)
+
+# -----------------------------------------------------------------------
+# Constants
+# -----------------------------------------------------------------------
+
+MIN_CHUNK_TOKENS = 300
+MAX_CHUNK_TOKENS = 800
+OVERLAP_TOKENS = 80
+SHORT_SECTION_TOKENS = 150
+MAX_MERGE_SECTIONS = 2
+
+_ENCODER = tiktoken.get_encoding("cl100k_base")
+
