5d7653f3d2765b47223c043faf848f0f7131d946
quantakavin <quantakavin@gmail.com>
2026-02-25 19:42:38 +0000

    update build citation method in chunk.py

--- rag_service/src/retrieval/query.py
+++ rag_service/src/retrieval/query.py
@@ -1,11 +1,5 @@
 from __future__ import annotations
 
-import hashlib
-import time
-from dataclasses import dataclass
-
-from sentence_transformers import SentenceTransformer
-
 from ..utils.logger import setup_logger
 
 logger = setup_logger(__name__)
