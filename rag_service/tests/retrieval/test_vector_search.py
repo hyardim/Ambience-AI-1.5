424e0aaebd65c0ede91bef41e8f6237a4da246af
quantakavin <quantakavin@gmail.com>
2026-02-25 22:45:14 +0000

    add helper constants for tests

--- rag_service/tests/retrieval/test_vector_search.py
+++ rag_service/tests/retrieval/test_vector_search.py
@@ -7,3 +7,10 @@ import pytest
 
 from src.retrieval.query import RetrievalError
 from src.retrieval.vector_search import VectorSearchResult, vector_search
+
+# -----------------------------------------------------------------------
+# Helpers
+# -----------------------------------------------------------------------
+
+VALID_EMBEDDING = [0.1] * 384
+WRONG_EMBEDDING = [0.1] * 100
